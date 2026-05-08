7698e07be13e1d16ae8cd356b892f989441a9678
isahwb <isabellisilva232@gmail.com>
2026-05-08 15:19:43 -0300

    .

--- imc.py
+++ imc.py
@@ -6,10 +6,11 @@ altura=float(input('Digite sua altura (m): '))
 imc = peso / (altura**2)
 print("Seu imc é: {:.2f}".format(imc))
 
-if imc >= 25: 
-    print("Você está acima do peso!")
+if imc >= 30: 
+    print("Você está com obesidade!")
+elif imc >= 25: 
+    print("Você está com sobrepeso!")
 elif imc >= 18.5: 
     print("Você está com peso normal!") 
 else: 
     print("Você está abaixo do peso!")
-    
